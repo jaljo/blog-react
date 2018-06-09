3ecc9d11de0c3ad694270b73439932b058ed6e77
Langlois <joris.langlois@gmail.com>
2018-06-09 21:25:28 +0200

    Slit up bootstrap theme.

--- src/App.js
+++ src/App.js
@@ -2,164 +2,48 @@ import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import Navbar from './components/NavBar.js';
+import BlogPost from './components/BlogPost.js';
+import Pagination from './components/Pagination.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import SideWidget from './components/SideWidget.js';
+import CategoriesWidget from './components/CategoriesWidget.js';
+import SearchWidget from './components/SearchWidget.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div class="container">
-              <a class="navbar-brand" href="#">Start Bootstrap</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                      <span class="sr-only">(current)</span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+          <Navbar/>
 
           <div class="container">
-
             <div class="row">
 
+              {/* main container */}
               <div class="col-md-8">
-
-                <h1 class="my-4">Page Heading
-                  <small>Secondary Text</small>
-                </h1>
-
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"/>
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    Posted on January 1, 2017 by
-                    <a href="#">Start Bootstrap</a>
-                  </div>
-                </div>
-
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"/>
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    Posted on January 1, 2017 by
-                    <a href="#">Start Bootstrap</a>
-                  </div>
-                </div>
-
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"/>
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    Posted on January 1, 2017 by
-                    <a href="#">Start Bootstrap</a>
-                  </div>
-                </div>
-
-                <ul class="pagination justify-content-center mb-4">
-                  <li class="page-item">
-                    <a class="page-link" href="#">&larr; Older</a>
-                  </li>
-                  <li class="page-item disabled">
-                    <a class="page-link" href="#">Newer &rarr;</a>
-                  </li>
-                </ul>
-
+                <Header/>
+                <BlogPost title="titre 1"/>
+                <BlogPost title="titre 2"/>
+                <BlogPost title="titre 3"/>
+                <Pagination/>
               </div>
+              {/* end main container */}
 
+              {/* side widgets container */}
               <div class="col-md-4">
-
-                <div class="card my-4">
-                  <h5 class="card-header">Search</h5>
-                  <div class="card-body">
-                    <div class="input-group">
-                      <input type="text" class="form-control" placeholder="Search for..."/>
-                      <span class="input-group-btn">
-                        <button class="btn btn-secondary" type="button">Go!</button>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card my-4">
-                  <h5 class="card-header">Categories</h5>
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-lg-6">
-                        <ul class="list-unstyled mb-0">
-                          <li>
-                            <a href="#">Web Design</a>
-                          </li>
-                          <li>
-                            <a href="#">HTML</a>
-                          </li>
-                          <li>
-                            <a href="#">Freebies</a>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-lg-6">
-                        <ul class="list-unstyled mb-0">
-                          <li>
-                            <a href="#">JavaScript</a>
-                          </li>
-                          <li>
-                            <a href="#">CSS</a>
-                          </li>
-                          <li>
-                            <a href="#">Tutorials</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card my-4">
-                  <h5 class="card-header">Side Widget</h5>
-                  <div class="card-body">
-                    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-                  </div>
-                </div>
-
+                <SearchWidget/>
+                <CategoriesWidget/>
+                <SideWidget/>
               </div>
+              {/* end side widgets container */}
 
             </div>
-
           </div>
 
-          <footer class="py-5 bg-dark">
-            <div class="container">
-              <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
-            </div>
-          </footer>
+          <Footer/>
+          
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1 class="my-4">Page Heading
+        <small>Secondary Text</small>
+      </h1>
+    );
+  }
+}
+
+export default Header;
