9a3e53c040ee6e3d9328665513f8b9440125ce50
Joris Langlois <joris.langlois@knplabs.com>
2018-06-23 21:44:58 +0200

    parse html content instead of escaping it

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from 'react'
 import moment from 'moment'
+import ReactHtmlParser from 'react-html-parser'
 
-// todo: parse html chain and create react elements
 export default ({post}) =>
   <div className="card mb-4">
     <div className="card-body">
       <h2 className="card-title">{ post.title }</h2>
-      <div dangerouslySetInnerHTML={ disableAutoescape(post.content) }>
+      <div className="content">
+        { ReactHtmlParser(post.content) }
       </div>
     </div>
     <div className="card-footer text-muted">
       Posted on { moment(post.date_creation).format('dddd D MMMM YYYY') }
     </div>
   </div>
-
-const disableAutoescape = rawHtml => ({ __html: rawHtml})
