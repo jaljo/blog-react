1b0be70d437242de52434897dbc2d9fc060fdbe9
Joris Langlois <joris.langlois@knplabs.com>
2020-03-09 14:15:14 +0100

    get rid of npm dependency

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+// getCurrentYear :: String
+const getCurrentYear = () => (new Date()).getFullYear()
+
 // Footer :: () -> React.Component
 export default () =>
   <footer className="py-5 bg-dark">
     <div className="container">
       <p className="m-0 text-center text-white">
-        Copyright &copy; Joris Langlois 2018
+        Copyright &copy; Joris Langlois {getCurrentYear()}
       </p>
     </div>
   </footer>
