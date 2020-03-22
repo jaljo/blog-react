6ef5601557f3d6560730142e178e1f45457846a4
Joris Langlois <joris.langlois@knplabs.com>
2020-03-22 20:36:00 +0100

    Add link to github repos

---
+++ src/Components/Router/Link/External/ExternalLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// ExternalLink :: Props -> React.Component
+export default ({
+  to,
+  children
+}) =>
+  <a
+    href={to}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {children}
+  </a>
